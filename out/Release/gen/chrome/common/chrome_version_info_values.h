4119c61da2f237a9612166a145b95c798845e95a
jianglong0156 <jianglong0156@gmail.com>
2015-06-26 18:34:18 +0800

    add version name

---
+++ out/Release/gen/chrome/common/chrome_version_info_values.h
@@ -0,0 +1,13 @@
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_COMMON_CHROME_VERSION_INFO_VALUES_H_
+#define CHROME_COMMON_CHROME_VERSION_INFO_VALUES_H_
+
+#define PRODUCT_NAME "Coco Simulator"
+#define PRODUCT_VERSION "42.0.2311.90"
+#define LAST_CHANGE "ca2c3c1e104182332bba1a241a9bde13a650a621"
+#define IS_OFFICIAL_BUILD 0
+
+#endif  // CHROME_COMMON_CHROME_VERSION_INFO_VALUES_H_
